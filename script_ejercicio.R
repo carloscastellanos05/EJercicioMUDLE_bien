819b7e9b132b51584c1cacd6278e5cb18cc8f4b4
Carlos Castellanos <carlos.castellanos@alumni.mondragon.edu>
2024-05-06 18:55:56 +0200

    calculamos medias por producto

--- script_ejercicio.R
+++ script_ejercicio.R
@@ -1,4 +1,9 @@
 library(readxl)
+library(dplyr)
 df<-read_xlsx("C://clases//Data science//reto4//ejercicioMUDLEbien//datos//online_retail_II.xlsx")
 str(df)
 length(unique(df$StockCode))
+
+df%>%
+  group_by(StockCode)%>%
+  summarise(mean=mean(Price,na.rm=T))
